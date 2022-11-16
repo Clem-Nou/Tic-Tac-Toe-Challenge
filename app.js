0204078a72aad39887aac7297797f0b02c64e130
Clément Nourricier <111963411+Clem-Nou@users.noreply.github.com>
2022-11-16 10:01:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// récupérer les éléments du DOM
-const cases = [...document.getElementsByClassName("case")]; // nodelist -> array
+const cases = [...document.getElementsByClassName("case")];
 let joueur = document.getElementById("joueur");
 let score1 = document.getElementById("score1");
 let score2 = document.getElementById("score2");
 let scoreNul = document.getElementById("scoreNul");
 
-// mémoire des stats du jeu
 let state = {
     joueurEnCours: 1,
     scoreJ1: 0,
@@ -37,27 +35,27 @@ const resetState = () => {
 
 const verifierVictoire = () => {
     if (
-        (state.c1 == state.c2 && state.c2 == state.c3 && state.c1 > 0) ||
-        (state.c1 == state.c4 && state.c4 == state.c7 && state.c1 > 0) ||
-        (state.c1 == state.c5 && state.c5 == state.c9 && state.c1 > 0) ||
-        (state.c3 == state.c5 && state.c5 == state.c7 && state.c7 > 0) ||
-        (state.c2 == state.c5 && state.c5 == state.c8 && state.c2 > 0) ||
-        (state.c3 == state.c6 && state.c6 == state.c9 && state.c3 > 0) ||
-        (state.c4 == state.c5 && state.c5 == state.c6 && state.c4 > 0) ||
-        (state.c7 == state.c8 && state.c8 == state.c9 && state.c7 > 0)
+        (state.c1 === state.c2 && state.c2 === state.c3 && state.c1 > 0) ||
+        (state.c1 === state.c4 && state.c4 === state.c7 && state.c1 > 0) ||
+        (state.c1 === state.c5 && state.c5 === state.c9 && state.c1 > 0) ||
+        (state.c3 === state.c5 && state.c5 === state.c7 && state.c7 > 0) ||
+        (state.c2 === state.c5 && state.c5 === state.c8 && state.c2 > 0) ||
+        (state.c3 === state.c6 && state.c6 === state.c9 && state.c3 > 0) ||
+        (state.c4 === state.c5 && state.c5 === state.c6 && state.c4 > 0) ||
+        (state.c7 === state.c8 && state.c8 === state.c9 && state.c7 > 0)
     ) {
         console.log("winner !");
         return true;
     } else if (
-        state.c1 !== 0 &&
-        state.c2 !== 0 &&
-        state.c3 !== 0 &&
-        state.c4 !== 0 &&
-        state.c5 !== 0 &&
-        state.c6 !== 0 &&
-        state.c7 !== 0 &&
-        state.c8 !== 0 &&
-        state.c9 !== 0
+        state.c1 !=== 0 &&
+        state.c2 !=== 0 &&
+        state.c3 !=== 0 &&
+        state.c4 !=== 0 &&
+        state.c5 !=== 0 &&
+        state.c6 !=== 0 &&
+        state.c7 !=== 0 &&
+        state.c8 !=== 0 &&
+        state.c9 !=== 0
     ) {
         return null;
     } else {
@@ -69,7 +67,7 @@ const jouerCase = (e) => {
     let idCase = e.target.id;
 
     // si case déjà jouée on ne fait rien
-    if (state[idCase] !== 0) return;
+    if (state[idCase] !=== 0) return;
 
     state[idCase] = state.joueurEnCours;
 
@@ -80,7 +78,7 @@ const jouerCase = (e) => {
 
         alert("Le gagnant est le joueur " + state.joueurEnCours);
 
-        if (state.joueurEnCours == 1) {
+        if (state.joueurEnCours === 1) {
             state.scoreJ1++;
             score1.textContent = state.scoreJ1;
         } else {
@@ -103,7 +101,7 @@ const jouerCase = (e) => {
         cases.forEach((c) => (c.textContent = ""));
     } else if (isVctoire === false) {
         // sinon on continue le jeu
-        if (state.joueurEnCours == 1) {
+        if (state.joueurEnCours === 1) {
             state.joueurEnCours = 2;
             e.target.textContent = "X";
             joueur.textContent = "2";
